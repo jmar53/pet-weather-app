0da12a0745ecdecb5a8fa49a635d1e46dbc6a907
Ryan <rmac335@sasktel.net>
2020-10-16 14:13:43 -0600

    Add functionality to add new pet

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const app = express();
 const request = require('request');
+const bodyParser = require('body-parser');
 const port = 4000;
 const petApiLink = 'http://localhost:3000/pets';
 
 app.set('view engine', 'pug');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   request.get(petApiLink, (error, response, body) => {
-    if(error) {
-        return console.dir(error);
+    if (error) {
+      return console.dir(error);
     }
     const pets = JSON.parse(body);
-    res.render('index', {pets: pets});
+    res.render('index', { pets: pets });
   });
 });
 
@@ -20,10 +23,26 @@ app.get('/new', (req, res) => {
   res.render('new', {});
 });
 
-app.post('/new', (req, res) =>{
+app.post('/new', (req, res) => {
   //ToDo: bodyParser
-  //const pet = req.body;
-  //console.log(pet);
+  const pet = req.body;
+  request.post({
+    "headers": { "content-type": "application/json" },
+    "url": petApiLink,
+    "body": JSON.stringify({
+      "name": pet.name,
+      "pet_type": pet.pet_type,
+      "breed": pet.breed,
+      "location": pet.location,
+      "latitude": pet.latitude,
+      "longitude": pet.longitude
+    })
+  }, (error, response, body) => {
+    if (error) {
+      return console.dir(error);
+    }
+    res.redirect('/');
+  });
 })
 
 app.listen(port, () => {
